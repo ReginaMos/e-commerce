75e4be8b06df8af4b808f89d709c73003227b27f
Yuri Skrypal <yjura@protonmail.com>
2025-05-06 13:57:48 +0300

    refactor: split address & other registration data

--- src/views/RegistrationView.vue
+++ src/views/RegistrationView.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { ref, reactive, computed } from 'vue';
-import { z } from 'zod';
+import { ref, reactive, computed, type ComputedRef } from 'vue';
+import { z, type AnyZodObject } from 'zod';
 import { countyList } from '../utils/country-list';
-import { registrationSchema, thirteenYearsAgo } from '../utils/registration-schema';
+import { addressSchema, registrationSchema, thirteenYearsAgo } from '../utils/registration-schema';
 
 type FormData = z.infer<typeof registrationSchema>;
+type AddressData = z.infer<typeof addressSchema>;
 
 const formData = reactive<FormData>({
   firstName: '',
@@ -12,34 +13,44 @@ const formData = reactive<FormData>({
   email: '',
   password: '',
   dateOfBirth: thirteenYearsAgo,
-  street: '',
+});
+const addressData = reactive<AddressData>({
   city: '',
-  postalCode: '',
   country: '',
+  postalCode: '',
+  streetName: '',
 });
 
 const form = ref(false);
 const menu = ref(false);
 const countries = ref(countyList);
 
-const getFieldRules = (fieldName: keyof FormData) => {
+const getFieldRules = <T extends object>(
+  fieldName: keyof T,
+  schema: AnyZodObject
+): ComputedRef<((v: string) => true | string)[]> => {
   return computed(() => [
     (v: string) => {
-      const result = registrationSchema.shape[fieldName].safeParse(v);
+      const result = schema.shape[fieldName].safeParse(v);
       return result.success ? true : result.error.issues[0].message;
     },
   ]);
 };
+
+const getFieldRulesForm = (fieldName: keyof FormData) => getFieldRules(fieldName, registrationSchema);
+const getFieldRulesAddress = (fieldName: keyof AddressData) => getFieldRules(fieldName, addressSchema);
 
 const formattedDate = computed(() => {
   return new Intl.DateTimeFormat().format(formData.dateOfBirth);
 });
 
 const register = async () => {
-  const result = registrationSchema.safeParse(formData);
+  const resultForm = registrationSchema.safeParse(formData);
+  const resultAddress = addressSchema.safeParse(addressData);
 
-  if (result.success) {
+  if (resultForm.success && resultAddress.success) {
     console.log('Registration data:', formData);
+    console.log('Address data:', addressData);
   }
 };
 </script>
@@ -53,7 +64,7 @@ const register = async () => {
           <v-form v-model="form" @submit.prevent="register">
             <v-text-field
               v-model="formData.firstName"
-              :rules="getFieldRules('firstName').value"
+              :rules="getFieldRulesForm('firstName').value"
               label="Username"
               variant="underlined"
               required
@@ -61,7 +72,7 @@ const register = async () => {
 
             <v-text-field
               v-model="formData.lastName"
-              :rules="getFieldRules('lastName').value"
+              :rules="getFieldRulesForm('lastName').value"
               label="Username"
               variant="underlined"
               required
@@ -69,7 +80,7 @@ const register = async () => {
 
             <v-text-field
               v-model="formData.email"
-              :rules="getFieldRules('email').value"
+              :rules="getFieldRulesForm('email').value"
               label="Email"
               type="email"
               variant="underlined"
@@ -78,7 +89,7 @@ const register = async () => {
 
             <v-text-field
               v-model="formData.password"
-              :rules="getFieldRules('password').value"
+              :rules="getFieldRulesForm('password').value"
               label="Password"
               type="password"
               variant="underlined"
@@ -95,12 +106,12 @@ const register = async () => {
               <template v-slot:activator="{ props }">
                 <v-text-field
                   v-model="formData.dateOfBirth"
+                  :rules="getFieldRulesForm('dateOfBirth').value"
                   :value="formattedDate"
                   label="Date of Birth"
                   prepend-icon="mdi-calendar"
                   variant="underlined"
                   v-bind="props"
-                  :rules="getFieldRules('dateOfBirth').value"
                   required
                 ></v-text-field>
               </template>
@@ -108,35 +119,35 @@ const register = async () => {
             </v-menu>
             <v-card-title class="text-start">Billing Address</v-card-title>
             <v-text-field
-              v-model="formData.street"
-              :rules="getFieldRules('street').value"
+              v-model="addressData.streetName"
+              :rules="getFieldRulesAddress('streetName').value"
               label="Street"
               variant="underlined"
               required
             ></v-text-field>
 
             <v-text-field
-              v-model="formData.city"
-              :rules="getFieldRules('city').value"
+              v-model="addressData.city"
+              :rules="getFieldRulesAddress('city').value"
               label="City"
               variant="underlined"
               required
             ></v-text-field>
 
             <v-text-field
-              v-model="formData.postalCode"
-              :rules="getFieldRules('postalCode').value"
+              v-model="addressData.postalCode"
+              :rules="getFieldRulesAddress('postalCode').value"
               label="Postal Code"
               variant="underlined"
               required
             ></v-text-field>
 
             <v-autocomplete
-              v-model="formData.country"
+              v-model="addressData.country"
               :items="countries"
               label="Country"
               variant="underlined"
-              :rules="getFieldRules('country').value"
+              :rules="getFieldRulesAddress('country').value"
               required
             ></v-autocomplete>
             <v-btn color="primary" type="submit" :disabled="!form"> Sign up </v-btn>
