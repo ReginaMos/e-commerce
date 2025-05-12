6c4b8282ee105f8876e78d41b6d4df1c44cb3809
Regina Mos <regishechka.m@gmail.com>
2025-05-12 21:44:57 +0500

    feat: add netlify.toml for deploy

--- src/router/index.ts
+++ src/router/index.ts
@@ -85,8 +85,8 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior() {
-    return { top: 0 }
-  }
+    return { top: 0 };
+  },
 });
 
 export default router;
