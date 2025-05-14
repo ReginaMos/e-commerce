49514cf89a1a2026971e98dc87a1a4b71516439d
Yuri Skripal <yjura@protonmail.com>
2025-05-14 18:36:50 +0300

    feat: add token cache & build client with password

---
+++ src/services/token-cache.ts
@@ -0,0 +1,17 @@
+import type { TokenCache } from '@commercetools/ts-client/dist/declarations/src/types/types';
+
+const TOKEN_KEY = 'vmt-token';
+
+export const tokenCache: TokenCache = {
+  get: () => {
+    const token = localStorage.getItem(TOKEN_KEY);
+    return token ? JSON.parse(token) : undefined;
+  },
+  set: (cache) => {
+    localStorage.setItem(TOKEN_KEY, JSON.stringify(cache));
+  },
+};
+
+export const clearToken = () => {
+  localStorage.removeItem(TOKEN_KEY);
+};
