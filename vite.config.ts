d9063cec6a30e19918748f84707a1dcf643c271f
Regina Mos <regishechka.m@gmail.com>
2025-05-14 19:09:20 +0500

    feat: add local files to promotion slider & add hash routing and deploy command

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 
 export default defineConfig({
+  base: '/e-commerce/release-sprint-2/',
   plugins: [vue(), vuetify({ autoImport: true })],
 });
