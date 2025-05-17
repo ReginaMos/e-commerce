75abfd1efbcf9a039e1408fd926b33de6bd10d95
Viktoriia Rosovska <torimeister@gmail.com>
2025-05-17 21:46:50 +0300

    fix: fix logout by USER_KEY, fix router redirect to account page, hide account page icon

--- src/router/index.ts
+++ src/router/index.ts
@@ -93,7 +93,7 @@ const router = createRouter({
 router.beforeEach(async (to) => {
   const isAuthenticated = localStorage.getItem(USER_KEY);
   if (!isAuthenticated && to.name === Links.USER.NAME) {
-    return { name: Links.SIGNUP.NAME}
+    return { name: Links.SIGNUP.NAME };
   }
   if (isAuthenticated && (to.name === Links.SIGNUP.NAME || to.name === Links.LOGIN.NAME)) {
     return { name: Links.HOME.NAME };
