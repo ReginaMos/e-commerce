41ff9fc46c1654f671b14fbbccfd6ed5182062cc
Yuri Skrypal <yjura@protonmail.com>
2025-05-22 09:13:54 +0300

    feat: user profile

---
+++ src/components/AddressBookComponent.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h2>Address Book</h2>
+  <p>Edit your delivery and billing address.</p>
+  <v-btn>Add New Address</v-btn>
+  <h3>Default Addresses</h3>
+  <h3>Other Addresses</h3>
+</template>
+<style scoped></style>
