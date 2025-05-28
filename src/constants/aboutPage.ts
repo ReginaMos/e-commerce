af418117450cde255e16092508135dd06de357f5
Viktoriia Rosovska <torimeister@gmail.com>
2025-05-28 21:49:01 +0300

    feat: add pictures

---
+++ src/constants/aboutPage.ts
@@ -0,0 +1,28 @@
+import type { PromotionModel } from '../models/models';
+import home from '../assets/about-page/home.png';
+import bag from '../assets/about-page/bag.png';
+import dollar from '../assets/about-page/dollar.png';
+import pack from '../assets/about-page/pack.png';
+
+export const aboutAdvantages: PromotionModel[] = [
+  {
+    heading: '10.5k',
+    text: 'Sallers active our site',
+    img: home,
+  },
+  {
+    heading: '33k',
+    text: 'Mopnthly Produduct Sale',
+    img: dollar,
+  },
+  {
+    heading: '45.5k',
+    text: 'Customer active in our site',
+    img: pack,
+  },
+  {
+    heading: '25k',
+    text: 'Anual gross sale in our site',
+    img: bag,
+  },
+];
---
+++ src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { aboutAdvantages } from '../constants/aboutPage.ts';
+</script>
+<template>
+  <v-container>
+    <v-row>
+      <v-col v-for="(advantage, ind) in aboutAdvantages" :key="ind">
+        <div>
+          <v-img :src="advantage.img" class="aboutAdvantagesImage" />
+          <h3>{{ advantage.heading }}</h3>
+          <p>{{ advantage.text }}</p>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<style scoped lang="scss">
+.aboutAdvantagesImage {
+  width: 80px;
+  height: 80px;
+  filter: invert(1);
+}
+</style>
