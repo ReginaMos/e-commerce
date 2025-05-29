12a1ab8d4b92e6fc51fa654e3ef29c68f5ea8973
Viktoriia Rosovska <torimeister@gmail.com>
2025-05-29 03:41:28 +0300

    feat: add image and styling hero container

---
+++ src/components/AboutAdvantagesComponent.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import { aboutAdvantages } from '../constants/aboutPage.ts';
+</script>
+<template>
+  <v-row class="advantage-wrapper">
+    <v-col v-for="(advantage, ind) in aboutAdvantages" :key="ind">
+      <div class="advantage-item">
+        <v-img :src="advantage.img" class="about-advantage-image" />
+        <h3>{{ advantage.heading }}</h3>
+        <p>{{ advantage.text }}</p>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+<style scoped lang="scss">
+.about-advantage-image {
+  width: 80px;
+  height: 80px;
+  margin-bottom: 24px;
+}
+.advantage-wrapper {
+  flex-wrap: wrap;
+  gap: 30px;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 140px;
+  @media screen and (min-width: 1400px) {
+    flex-wrap: nowrap;
+  }
+}
+.advantage-wrapper h3 {
+  margin-bottom: 8px;
+}
+.advantage-item:hover .about-advantage-image {
+  filter: invert(1);
+}
+.advantage-item {
+  border: 1px solid var(--gray-secondary);
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  padding: 30px;
+  width: 270px;
+  height: 230px;
+  font-size: 14px;
+
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  &:hover,
+  &:focus,
+  &:active {
+    background-color: var(--red-secondary);
+    color: var(--white-text);
+    border-color: var(--red-secondary);
+    box-shadow: rgba(0, 0, 0, 0.4) 0px 3px 8px;
+  }
+  @media screen and (min-width: 578px) {
+    font-size: 16px;
+  }
+}
+.advantage-item .aboutAdvantageImage {
+  &:hover,
+  &:focus,
+  &:active {
+    filter: invert(1);
+  }
+}
+
+.advantage-wrapper :deep(.v-col) {
+  width: 270px;
+  height: 230px;
+  max-width: 270px;
+  max-height: 230px;
+  justify-content: center;
+}
+.v-col {
+  padding: 0;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,23 +1,75 @@
 <script setup lang="ts">
-import { aboutAdvantages } from '../constants/aboutPage.ts';
+import AboutAdvantagesComponent from '../components/AboutAdvantagesComponent.vue';
+import slideAbout from '../assets/about-page/slideAbout.png';
 </script>
 <template>
   <v-container>
-    <v-row>
-      <v-col v-for="(advantage, ind) in aboutAdvantages" :key="ind">
-        <div>
-          <v-img :src="advantage.img" class="aboutAdvantagesImage" />
-          <h3>{{ advantage.heading }}</h3>
-          <p>{{ advantage.text }}</p>
-        </div>
+    <v-row class="about-hero">
+      <v-col class="about-hero-content">
+        <h2 class="about-hero-heading">Our Story</h2>
+        <p>
+          Launced in 2015, Exclusive is South Asia’s premier online shopping makterplace with an active presense in
+          Bangladesh. Supported by wide range of tailored marketing, data and service solutions, Exclusive has 10,500
+          sallers and 300 brands and serves 3 millioons customers across the region.
+        </p>
+        <p>
+          Exclusive has more than 1 Million products to offer, growing at a very fast. Exclusive offers a diverse
+          assotment in categories ranging from consumer.
+        </p>
+      </v-col>
+      <v-col>
+        <v-img :src="slideAbout" class="slide-about-image"/>
       </v-col>
     </v-row>
+    <AboutAdvantagesComponent />
   </v-container>
 </template>
 <style scoped lang="scss">
-.aboutAdvantagesImage {
-  width: 80px;
-  height: 80px;
-  filter: invert(1);
+.v-col {
+  padding: 0;
+}
+.about-hero{
+ gap: 30px;
+ flex-direction: column;
+ margin-bottom: 50px;
+ @media screen and (min-width: 578px){
+  /* gap: 50px; */
+ }
+ @media screen and (min-width: 1200px){
+  flex-direction: row;
+  margin-bottom: 140px;
+ }
+
+}
+.about-hero-content {
+ display: flex;
+ flex-direction: column;
+ justify-content: flex-start;
+ text-align: left;
+ gap: 40px;
+ 
+ font-size: 16px;
+ 
+ @media screen and (min-width: 1200px){
+  padding-top: 137px;
+ padding-right: 75px;
+ padding-bottom: 137px;
+ width: 525px;
+ 
+ }
+}
+.about-hero-heading {
+  font-size: 54px;
+  line-height: 64px;
+  letter-spacing: 0.06;
+  font-weight: 500;
+}  
+.slide-about-image {
+@media screen and (min-width: 1200px){
+  position: absolute;
+   right: 0;
+   max-width: 705px;
+   min-width: 50%;
+ }
 }
 </style>
